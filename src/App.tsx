33f93d1b278dfceafb622b390c025c1be71197c1
Jesse Looney <42242696+jesselooney@users.noreply.github.com>
2025-07-03 20:13:55 -0400

    remove feather icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
 import PWABadge from './PWABadge.tsx'
 import { A } from '@solidjs/router'
 import styles from './App.module.css'
-import featherSpriteUrl from '/public/feather-sprite.svg'
-
-const clockIconUrl = featherSpriteUrl + "#clock"
-const eyeIconUrl = featherSpriteUrl + "#eye"
 
 function App(props: any) {
   return (
@@ -17,15 +13,9 @@ function App(props: any) {
       </main>
       <nav class={styles.Navigation}>
         <A href="/track" activeClass={`${styles.NavItem} ${styles.NavItem__Active}`} inactiveClass={styles.NavItem}>
-          <svg class="feather">
-            <use href={clockIconUrl} />
-          </svg>
           <span>Track</span>
         </A>
         <A href="/analyze" activeClass={`${styles.NavItem} ${styles.NavItem__Active}`} inactiveClass={styles.NavItem}>
-          <svg class="feather">
-            <use href={eyeIconUrl} />
-          </svg>
           <span>Analyze</span>
         </A>
       </nav >
