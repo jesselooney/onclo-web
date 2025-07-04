f9c84fee6e01241ec98eec3dc1afbf4ab2cab77c
Jesse Looney <42242696+jesselooney@users.noreply.github.com>
2025-07-03 21:25:22 -0400

    try manual screen resizing

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import PWABadge from './PWABadge.tsx'
 import { A } from '@solidjs/router'
+import { onCleanup, onMount } from 'solid-js'
 import styles from './App.module.css'
 
 function App(props: any) {
+  const updateHeight = () => document.getElementById("root")!.style.height = `${window.innerHeight}px`
+
+  onMount(() => window.addEventListener("resize", updateHeight))
+  onCleanup(() => window.removeEventListener("resize", updateHeight))
+
   return (
     <>
       <header class={styles.Header}>
